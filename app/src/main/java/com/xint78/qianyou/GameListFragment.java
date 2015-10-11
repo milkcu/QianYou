1017ea2c4744629a2b227e88c24fc446059bb364
Xintong Liu <xint78@gmail.com>
2015-10-12 01:38:52 +0800

    Implement the effect of click and scroll using RadioGroup and ViewPager

---
+++ app/src/main/java/com/xint78/qianyou/GameListFragment.java
@@ -0,0 +1,125 @@
+package com.xint78.qianyou;
+
+/*
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GameListFragment extends Fragment {
+
+    private int tabIndex = 0;
+
+    //private OnFragmentInteractionListener mListener;
+
+    // TODO: Rename and change types and number of parameters
+    public static GameListFragment newInstance(int index) {
+        GameListFragment fragment = new GameListFragment();
+        Bundle args = new Bundle();
+        args.putInt("index", index);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /*
+    public GameListFragment() {
+        // Required empty public constructor
+    }
+    */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        tabIndex = bundle.getInt("index");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_list, container, false);
+    }
+/*
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+        //Toast.makeText(getActivity(), "index - " + tabIndex, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+*/
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    /*
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+    */
+/*
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+        switch (tabIndex) {
+            case 0:
+                Toast.makeText(getActivity(), "您点击了地0个书签", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(getActivity(), "您点击了地1个书签", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(getActivity(), "您点击了地2个书签", Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                Toast.makeText(getActivity(), "您点击了地3个书签", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+*/
+}
--- app/src/main/java/com/xint78/qianyou/MainActivity.java
+++ app/src/main/java/com/xint78/qianyou/MainActivity.java
@@ -1,13 +1,136 @@
 package com.xint78.qianyou;
-
+/*
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+*/
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Gravity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager viewPager_main;
+    private RadioGroup radioGroup_main;
+    private List<RadioButton> list;
+    private List<Fragment> listFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initTab();
+        initData();
+        initListener();
+    }
+
+    private void initView() {
+        // TODO Auto-generated method stub
+        viewPager_main = (ViewPager) findViewById(R.id.viewPager_main);
+        radioGroup_main = (RadioGroup) findViewById(R.id.radioGroup_main);
+    }
+
+    private void initTab() {
+        // TODO Auto-generated method stub
+        list = new ArrayList<RadioButton>();
+        String[] tabArr = getResources().getStringArray(R.array.tabName);
+        int len = tabArr.length;
+        for (int i = 0; i < len; i++) {
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(tabArr[i]);
+            radioButton.setGravity(Gravity.CENTER);
+            radioButton.setButtonDrawable(R.drawable.radiobutton_selector);
+            //radioButton.setBackgroundResource(R.drawable.partition);
+            int length = getResources().getDisplayMetrics().widthPixels;
+            int eachLength = length / len;
+            radioButton.setWidth(eachLength);
+            radioGroup_main.addView(radioButton);
+            list.add(radioButton);
+        }
+    }
+
+    private void initData() {
+        // TODO Auto-generated method stub
+        listFragment = new ArrayList<Fragment>();;
+        int length = list.size();
+        for (int i = 0; i < length; i++) {
+            GameListFragment frag = GameListFragment.newInstance(i);
+            listFragment.add(frag);
+        }
+        FragmentManager man = getSupportFragmentManager();
+        viewPager_main.setAdapter(new MyViewPager(man, listFragment));
+    }
+
+    private void initListener() {
+        // TODO Auto-generated method stub
+        radioGroup_main
+                .setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        // TODO Auto-generated method stub
+                        for (int i = 0; i < radioGroup_main.getChildCount(); i++) {
+                            if (list.get(i).isChecked()) {
+                                viewPager_main.setCurrentItem(i);
+                            }
+                        }
+                    }
+                });
+        viewPager_main.setOnPageChangeListener(new OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int arg0) {
+                // TODO Auto-generated method stub
+                list.get(arg0).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        list.get(0).setChecked(true);
+    }
+
+    class MyViewPager extends FragmentPagerAdapter {
+        private List<Fragment> list;
+
+        public MyViewPager(FragmentManager fm, List<Fragment> list) {
+            super(fm);
+            // TODO Auto-generated constructor stub
+            this.list = list;
+        }
+
+        @Override
+        public Fragment getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return list.get(arg0);
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return list.size();
+        }
+
     }
 }
